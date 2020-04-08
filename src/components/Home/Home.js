65c8375a7bf7710d7a72b3dfe15967f988e17f2c
Nick Incardone <nickincardone@gmail.com>
2020-04-07 22:30:08 -0500

    improved styling of portfolio tab, added github action button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,7 +17,7 @@ import Portfolio from './Portfolio/Portfolio';
 
 function Home() {
   const location = useLocation();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(3);
   if (value !== 2 && location.hash === '#resume') {
     setValue(2);
   }
@@ -70,7 +70,7 @@ function Home() {
           <Tab label="Travel" {...a11yProps(0)} />
           <Tab label="Playlists" {...a11yProps(1)} />
           <Tab label="Resume" {...a11yProps(2)} />
-          {/*<Tab label="Portfolio" {...a11yProps(3)} />*/}
+          <Tab label="Portfolio" {...a11yProps(3)} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
--- src/components/Home/Portfolio/Portfolio.js
+++ src/components/Home/Portfolio/Portfolio.js
@@ -4,7 +4,10 @@ import { Card } from '@material-ui/core';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import profilePic from "../../../images/prof.jpg";
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import { GitHub } from '@material-ui/icons';
+import './Portfolio.scss';
 
 const portfolio = (props) => {
 
@@ -12,16 +15,32 @@ const portfolio = (props) => {
     <Grid className="nji-temp nji-portfolio" container spacing={5}>
       <Grid item xs={12} md={6}>
         <Card>
-          <CardHeader title="Website" />
+          <CardHeader title="Website"/>
           <CardMedia image="http://nickincardone.com/images/website.png"/>
-          <CardContent>This site right here</CardContent>
+          <CardContent>This site right here. A static React application that generates my website
+            from some JSON files.</CardContent>
+          <CardActions disableSpacing>
+            <a href="http://nickincardone.com">
+              <IconButton aria-label="go to repo">
+                <GitHub/>
+              </IconButton>
+            </a>
+          </CardActions>
         </Card>
       </Grid>
       <Grid item xs={12} md={6}>
         <Card>
-          <CardHeader title="Conjugator" />
+          <CardHeader title="Conjugator"/>
           <CardMedia image="http://nickincardone.com/images/conjugator.png"/>
-          <CardContent>This site right here</CardContent>
+          <CardContent>Practice Spanish conjugations and definitions with over 1000 verbs. MVP of a
+            larger Spanish App that I am working on. </CardContent>
+          <CardActions disableSpacing>
+            <a href="http://nickincardone.com">
+              <IconButton aria-label="go to repo">
+                <GitHub/>
+              </IconButton>
+            </a>
+          </CardActions>
         </Card>
       </Grid>
     </Grid>
