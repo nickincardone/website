6be7add74effd4a0327617fb79202e2b1b028f7f
Nick Incardone <nickincardone@gmail.com>
2020-04-13 16:27:54 -0400

    fixed conjugator github link

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,7 +4,7 @@ const portfolio = [
     "description": "Practice Spanish conjugations and definitions with over 1000 verbs. MVP of a\n" +
       "            larger Spanish Application that I am working on.",
     "image": "http://nickincardone.com/images/conjugator.png",
-    "githubLink": "https://github.com/nickincardone/poropara",
+    "githubLink": "https://github.com/nickincardone/conjugator",
     "demoLink": "http://nickincardone.com/conjugator/",
     "technologies": ['React']
   }, {
